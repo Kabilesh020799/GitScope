9f9228853c2e246705f6a3fbfae9b96f6f56f85b
Kabilesh <kb320903@dal.ca>
2025-05-02 00:02:40 -0300

    refactored login background component

---
+++ src/containers/login/components/login-background/constants.js
@@ -0,0 +1,71 @@
+const PARTICLES_OPTIONS = {
+  background: {
+    color: {
+      value: "#040E21",
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#8193b2",
+    },
+    links: {
+      color: "#8193b2",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+
+export { PARTICLES_OPTIONS };
--- src/containers/login/components/login-background/loginBackground.jsx
+++ src/containers/login/components/login-background/loginBackground.jsx
@@ -1,93 +1,29 @@
 import { useEffect, useState } from "react";
-import React from 'react';
+import React from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim"; 
+import { loadSlim } from "@tsparticles/slim";
+import { PARTICLES_OPTIONS } from "./constants";
 
 const LoginBackground = () => {
-  const [ init, setInit ] = useState(false);
+  const [isParticlesLoaded, setIsParticlesLoaded] = useState(false);
 
   useEffect(() => {
-		initParticlesEngine(async (engine) => {
-			await loadSlim(engine);
-		}).then(() => {
-			setInit(true);
-		});
-	}, []);
+    const initParticles = async () => {
+      initParticlesEngine(async (engine) => {
+        await loadSlim(engine);
+      }).then(() => {
+        setIsParticlesLoaded(true);
+      });
+    };
 
-	return (
-		init && <Particles
-			id="tsparticles"
-			options={{
-				background: {
-						color: {
-								value: "#040E21",
-						},
-				},
-				fpsLimit: 120,
-				interactivity: {
-					events: {
-						onClick: {
-							enable: true,
-							mode: "push",
-						},
-						onHover: {
-							enable: true,
-							mode: "repulse",
-						},
-						resize: true,
-					},
-					modes: {
-						push: {
-							quantity: 4,
-						},
-						repulse: {
-							distance: 200,
-							duration: 0.4,
-						},
-						},
-				},
-				particles: {
-					color: {
-						value: "#8193b2",
-					},
-					links: {
-						color: "#8193b2",
-						distance: 150,
-						enable: true,
-						opacity: 0.5,
-						width: 1,
-					},
-					move: {
-						direction: "none",
-						enable: true,
-						outModes: {
-							default: "bounce",
-						},
-						random: false,
-						speed: 6,
-						straight: false,
-					},
-					number: {
-						density: {
-							enable: true,
-							area: 800,
-						},
-						value: 80,
-					},
-					opacity: {
-						value: 0.5,
-					},
-					shape: {
-						type: "circle",
-					},
-					size: {
-						value: { min: 1, max: 5 },
-					},
-				},
-				detectRetina: true,
-			}}
-		/>
-	);
+    initParticles();
+  }, []);
+
+  return (
+    isParticlesLoaded && (
+      <Particles id="tsparticles" options={PARTICLES_OPTIONS} />
+    )
+  );
 };
 
-export default LoginBackground;
+export default React.memo(LoginBackground);
