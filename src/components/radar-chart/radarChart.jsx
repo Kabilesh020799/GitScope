c0f3dbe8f310f2ed29f10b1a86a42e07966b87cd
Kabilesh <kb320903@dal.ca>
2025-05-02 01:35:31 -0300

    user contributions refactor

--- src/components/radar-chart/radarChart.jsx
+++ src/components/radar-chart/radarChart.jsx
@@ -1,30 +1,34 @@
 import React, { useEffect } from "react";
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
 const RadarChart = (props) => {
-  const {
-    data,
-  } = props;
+  const { data } = props;
 
   useEffect(() => {
-    const width = 500, height = 500;
-    const margin = {top: 50, right: 80, bottom: 50, left: 80},
-          chartRadius = Math.min(width, height) / 2 - Math.max(...Object.values(margin));
+    const width = 500,
+      height = 500;
+    const margin = { top: 50, right: 80, bottom: 50, left: 80 },
+      chartRadius =
+        Math.min(width, height) / 2 - Math.max(...Object.values(margin));
 
-    const svg = d3.select(".radar-chart").append("svg")
+    const svg = d3
+      .select(".radar-chart")
+      .append("svg")
       .attr("width", width)
       .attr("height", height)
       .append("g")
-      .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     // Create the straight lines radiating from the center
-    const radarLine = d3.lineRadial()
+    const radarLine = d3
+      .lineRadial()
       .curve(d3.curveLinearClosed)
-      .radius(d => d.value * chartRadius)
-      .angle((d,i) => i * 2 * Math.PI / data.length);
+      .radius((d) => d.value * chartRadius)
+      .angle((d, i) => (i * 2 * Math.PI) / data.length);
 
     // Append the backgrounds
-    svg.append("path")
+    svg
+      .append("path")
       .datum(data)
       .attr("d", radarLine)
       .attr("stroke", "darkblue")
@@ -33,36 +37,60 @@ const RadarChart = (props) => {
 
     // Create the axes
     data.forEach((d, i) => {
-    svg.append("line")
-      .attr("x1", 0)
-      .attr("y1", 0)
-      .attr("x2", chartRadius * Math.cos(i * 2 * Math.PI / data.length - Math.PI / 2))
-      .attr("y2", chartRadius * Math.sin(i * 2 * Math.PI / data.length - Math.PI / 2))
-      .attr("stroke", "lightgray");
+      svg
+        .append("line")
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr(
+          "x2",
+          chartRadius * Math.cos((i * 2 * Math.PI) / data.length - Math.PI / 2)
+        )
+        .attr(
+          "y2",
+          chartRadius * Math.sin((i * 2 * Math.PI) / data.length - Math.PI / 2)
+        )
+        .attr("stroke", "lightgray");
 
-    // Add axis labels
-    svg.append("text")
-      .attr("x", (chartRadius + 10) * Math.cos(i * 2 * Math.PI / data.length - Math.PI / 2))
-      .attr("y", (chartRadius + 10) * Math.sin(i * 2 * Math.PI / data.length - Math.PI / 2))
-      .attr("text-anchor", "middle")
-      .text(d.axis)
-      .style('fill', '#555963');
+      // Add axis labels
+      svg
+        .append("text")
+        .attr(
+          "x",
+          (chartRadius + 10) *
+            Math.cos((i * 2 * Math.PI) / data.length - Math.PI / 2)
+        )
+        .attr(
+          "y",
+          (chartRadius + 10) *
+            Math.sin((i * 2 * Math.PI) / data.length - Math.PI / 2)
+        )
+        .attr("text-anchor", "middle")
+        .text(d.axis)
+        .style("fill", "#555963");
     });
 
     // Draw the points
-    svg.selectAll(".point")
-    .data(data)
-    .enter().append("circle")
-      .attr("cx", d => chartRadius * d.value * Math.cos(radarLine.angle()(d) - Math.PI / 2))
-      .attr("cy", d => chartRadius * d.value * Math.sin(radarLine.angle()(d) - Math.PI / 2))
+    svg
+      .selectAll(".point")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr(
+        "cx",
+        (d) =>
+          chartRadius * d.value * Math.cos(radarLine.angle()(d) - Math.PI / 2)
+      )
+      .attr(
+        "cy",
+        (d) =>
+          chartRadius * d.value * Math.sin(radarLine.angle()(d) - Math.PI / 2)
+      )
       .attr("r", 4)
       .attr("fill", "darkblue");
-    return () => d3.select('.radar-chart').html('');
+    return () => d3.select(".radar-chart").html("");
   }, []);
 
-  return (
-    <div className="radar-chart" />
-  );
+  return <div className="radar-chart" />;
 };
 
-export default RadarChart;
+export default React.memo(RadarChart);
--- src/containers/user-contributions/userContributions.jsx
+++ src/containers/user-contributions/userContributions.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import RadarChart from "../../components/radar-chart";
 import { fetchGitHubData } from "./apiUtils";
-import { CircularProgress, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import './style.scss';
+import {
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import "./style.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { replaceCollaborators } from "../dashboard/reducer";
 import { getAllCollaborators } from "../contributor-activity/apiUtils";
@@ -10,86 +16,112 @@ import { useNavigate } from "react-router-dom";
 
 const UserContributions = () => {
   const dispatch = useDispatch();
-  const { collaborators } = useSelector( state => state.commitReducer );
-  const [dropdownValue, setDropdownValue] = useState('');
+  const { collaborators } = useSelector((state) => state.commitReducer);
+  const [dropdownValue, setDropdownValue] = useState("");
   const [radarData, setRadarData] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const onClickDashboard = () => {
-    navigate('/dashboard');
-  };
+  const onClickDashboard = useCallback(() => {
+    navigate("/dashboard");
+  }, [navigate]);
 
   useEffect(() => {
     setLoading(true);
     getAllCollaborators()
       .then((res) => {
-        if(res.status !== 403) {
+        if (res.status !== 403) {
           dispatch(replaceCollaborators({ data: res }));
         }
       })
-      .catch(() => {
-        setLoading(false);
+      .catch((err) => {
+        console.error(err);
       })
       .finally(() => {
         setLoading(false);
       });
   }, []);
 
-  const onSelectDropdown = (value) => {
-    setDropdownValue(value.target.value);
+  const onSelectDropdown = useCallback((value) => {
+    const selectedUser = value.target.value;
+    setDropdownValue(selectedUser);
     setLoading(true);
-    fetchGitHubData(value.target.value)
+    fetchGitHubData(selectedUser)
       .then((res) => {
-        const radarChartData = [
-          {axis: "Commits", value: res.commits},
-          {axis: "Pull Requests", value: res.pullRequests},
-          {axis: "Code Reviews", value: res.codeReviews}
-        ];
-        setRadarData(radarChartData);
+        setRadarData([
+          { axis: "Commits", value: res.commits },
+          { axis: "Pull Requests", value: res.pullRequests },
+          { axis: "Code Reviews", value: res.codeReviews },
+        ]);
+      })
+      .catch((err) => {
+        console.error(err);
       })
       .finally(() => {
         setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
       });
-  };
+  }, []);
 
   return (
     <div className="user-contributions">
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '50px', marginBottom: '50px', marginTop: '30px' }}>
-        <h1 className="user-contributions-heading">Get to know about each User&apos;s contribution</h1>
-        <button
-          className="commit-activity-btn"
-          onClick={onClickDashboard}
-        >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "50px",
+          marginBottom: "50px",
+          marginTop: "30px",
+        }}
+      >
+        <h1 className="user-contributions-heading">
+          Get to know about each User&apos;s contribution
+        </h1>
+        <button className="commit-activity-btn" onClick={onClickDashboard}>
           Go to Dashboard
         </button>
       </div>
-        <FormControl className="user-contributions-dropdown">
-          <InputLabel id="demo-simple-select-label" style={{ color: '#8193b2' }}>Select User</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="Select User"
-            onChange={onSelectDropdown}
-            value={dropdownValue}
-            style={{ color: '#8193b2' }}
-          >
-            {collaborators?.map((collaborator) => (
-              <MenuItem key={collaborator?.author?.login} value={collaborator?.author?.login}>{collaborator?.author?.login}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        { dropdownValue ? <span style={{ color: '#8193b2' }}>Blue Line indicates the user contribution</span> : null }
-      { loading ? (
-        <div style={{ height: 'calc(100% - 100px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <FormControl className="user-contributions-dropdown">
+        <InputLabel id="demo-simple-select-label" style={{ color: "#8193b2" }}>
+          Select User
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Select User"
+          onChange={onSelectDropdown}
+          value={dropdownValue}
+          style={{ color: "#8193b2" }}
+        >
+          {collaborators?.map((collaborator) => (
+            <MenuItem
+              key={collaborator?.author?.login}
+              value={collaborator?.author?.login}
+            >
+              {collaborator?.author?.login}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {dropdownValue ? (
+        <span style={{ color: "#8193b2" }}>
+          Blue Line indicates the user contribution
+        </span>
+      ) : null}
+      {loading ? (
+        <div
+          style={{
+            height: "calc(100% - 100px)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <CircularProgress />
         </div>
-      ) : (
-        radarData ? (<RadarChart data={radarData} />) : null
-      ) }
+      ) : radarData ? (
+        <RadarChart data={radarData} />
+      ) : null}
     </div>
   );
 };
